6a724ce14c82bd65158999669f7de9bdd61ee154
ManishGupta <139673041+ManishGupta2027@users.noreply.github.com>
2025-03-12 08:11:14 +0530

    add user entity

---
+++ Core/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿
+using Microsoft.AspNet.Identity.EntityFramework;
+namespace Core.Entities
+{
+    public class User : IdentityUser
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+    }
+}
